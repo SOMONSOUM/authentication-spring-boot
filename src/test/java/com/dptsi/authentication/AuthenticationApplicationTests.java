1cafe888db602025b012749d1628e236ba2a9e67
SOUM SOM ON <somon.soum9@gmail.com>
2025-10-27 16:22:54 +0700

    Initial commit

---
+++ src/test/java/com/dptsi/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dptsi.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
